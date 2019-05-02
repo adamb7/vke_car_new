b913681490beccf23d523504479df3426b11fd3d
adamb7 <32942300+adamb7@users.noreply.github.com>
2019-05-02 11:47:17 +0200

    Add files via upload

--- current_measure_final/LipoMonitor/LipoMonitor.py
+++ current_measure_final/LipoMonitor/LipoMonitor.py
@@ -8,7 +8,7 @@ from IRSensor import IRSensor as ir
 MAX_CURRENT     = 5  # 5 AMPER
 R_SHUNT         = 0.1  # 100 mOHM
 CURRENT_LSB     = MAX_CURRENT / 32768
-CAL = 337
+CAL = 0.00512 / (CURRENT_LSB * R_SHUNT)
 CLEAR_FAULTS 		    = 0x03
 RESTORE_DEFAULT_ALL 	= 0x12
 CAPABILITY 		        = 0x19
@@ -49,13 +49,14 @@ monitor_sampling_rate = 5
 data_readings = {
     "current": 0,
     "voltage": 0,
-    "power"	:	0
+    "power"	:	0,
+    "shunt":   0
 }
 
 
 
 def init_ina233():
-    global address, CAL_HEX
+    global address
     try:
         bus.write_byte(address, CLEAR_FAULTS)
         bus.write_word_data(address,MFR_CALIBRATION, CAL)
@@ -95,12 +96,22 @@ def read_power():
         print e
     return power_data
 
+def read_shunt():
+    global address ,bus
+    try:
+        shunt_data = bus.read_word_data(address, MFR_READ_VSHUNT)
+    except Exception, e:
+        print "Voltage read error"
+        print "Address: %s" % address
+        print e
+    return shunt_data
+
 def calculate_measurement(source):
     global CURRENT_LSB
     R 		= 0
     b 		= 0
     try:
-        if source != "voltage":
+        if (source != "voltage") & (source != "shunt") :
             if source == "current":     # source = current
                 m = 1 / CURRENT_LSB
                 Y = read_current()
@@ -114,9 +125,14 @@ def calculate_measurement(source):
                 m /= 10
                 R -= 1
         else:                           # source = voltage
-            m = 8  # 1.25 mV/bit
-            R = 2
-            Y = read_voltage()
+            if source == "voltage":
+                m = 8  # 1.25 mV/bit
+                R = 2
+                Y = read_voltage()
+            else:
+                m = 4 # 2.5 uV/bit
+                R = 5
+                Y = read_shunt()
         X = (1 / m) * ((Y * (pow(10,-R)))  - b)
         return X
     except Exception, e:
@@ -131,6 +147,7 @@ def lipo_read_loop():
         data_readings["current"] = calculate_measurement("current")
         data_readings["voltage"] = calculate_measurement("voltage")
         data_readings["power"] 	 = calculate_measurement("power")
+        data_readings["shunt"]   = calculate_measurement("shunt")
         time.sleep(monitor_sampling_rate)
 
 def StartLipoMonitorSampling():
@@ -144,13 +161,14 @@ def get_readings():
 # #"""FOR TESTING"""
 # if __name__ == '__main__':
 #     try:
-#         init_ina233()
-#         print("Voltage in V:")
-#         print(calculate_measurement("voltage"))
-#         print("Current in A:")
-#         print(calculate_measurement("current"))
-#         print("Power in W:")
-#         print(calculate_measurement("power"))
+#         #init_ina233()
+#         print(CAL)
+#         # print("Voltage in V:")
+#         # print(calculate_measurement("voltage"))
+#         # print("Current in A:")
+#         # print(calculate_measurement("current"))
+#         # print("Power in W:")
+#         # print(calculate_measurement("power"))
 #     except Exception, e:
 #         print "Test failed"
 #         print e
