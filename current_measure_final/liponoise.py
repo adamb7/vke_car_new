dba04c77497844f91d12206cc18242c898a4db68
adamb7 <32942300+adamb7@users.noreply.github.com>
2019-05-23 16:11:41 +0200

    Add files via upload

--- current_measure_final/liponoise.py
+++ current_measure_final/liponoise.py
@@ -13,28 +13,35 @@ def liponoise():
     try:
         i = 0
         j = 0
-        data_array = 100 * [0]
+        data_array = 300 * [0]
         time_now = datetime.datetime.now()
         f = open("liponoise.log", 'w')
+        f.write("Timestamp     \tVoltage\tCurrent\n")
         while True:
-            while i < 100:
-                data = lipo.calculate_measurement("voltage")
+            while i < 300:
                 date_data = datetime.datetime.now() - time_now
                 data_array[i] = unicode(date_data)
                 i = i + 1
+                data = lipo.calculate_measurement("voltage")
+                data_array[i] = data
+                i = i + 1
+                data = lipo.calculate_measurement("current")
                 data_array[i] = data
                 i = i + 1
                 time.sleep(0.05)
             i = 0
             j = j + 1
             k = 0
+            print("j erteke: ")
+            print(j)
             for item in data_array:
-                f.write("%s " % item)
+                f.write("%s\t" % item)
                 k = k + 1
-                if k % 2 == 0:
+                if k % 3 == 0:
                     f.write("\n")
-            if j == 100:
+            if j == 100: #4:05 /6 kor
                 f.close()
+                print("end")
                 break
 
     except Exception, e:
@@ -42,7 +49,7 @@ def liponoise():
         print(e)
         f.close()
 
-#if __name__ == '__main__':
-#    liponoise()
+if __name__ == '__main__':
+    liponoise()
 
 
